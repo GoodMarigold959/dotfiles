58068f63c98679197e345e1d7b1e57fa7fdfe2c8
Oboe Reed <31904648+GoodMarigold959@users.noreply.github.com>
2024-06-23 14:04:24 -0500

    Update config.fish

--- config.fish
+++ config.fish
@@ -8,7 +8,11 @@ end
 function lfcd --wraps="lf" --description="lf - Terminal file manager (changing directory on exit)"
     # `command` is needed in case `lfcd` is aliased to `lf`.
     # Quotes will cause `cd` to not change directory if `lf` prints nothing to stdout due to an error.
-    cd "$(command lf -print-last-dir $argv)"
+    z "$(command lf -print-last-dir $argv)"
 end
 
+alias code "code -n --disable-extensions"
 alias lf "lfcd"
+alias box "z ~/University\ of\ Michigan \Dropbox/Daniel"
+
+zoxide init fish | source
